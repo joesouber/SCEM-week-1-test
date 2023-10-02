44aa4d6d231828b3503c8428369fd1ce8471d337
joesouber <78925833+joesouber@users.noreply.github.com>
2023-10-02 15:13:59 +0100

    dataframe practice

---
+++ dataframepractice.R
@@ -0,0 +1,17 @@
+# Create the vectors
+animals <- c("Snake" , "Ostrich", "Cat", "Spider")
+num_legs <- c("0","2","4","8")
+
+print(animals)
+print(num_legs)
+
+
+# Create the data frame
+animals_df <- data.frame(Animal = animals, NumLegs = num_legs)
+
+# Print the data frame
+print(animals_df)
+
+
+# Option/Command/R to run all... alternatively can highlight all and Run.
+
